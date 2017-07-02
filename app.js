7593459a4f29d3e4b527371cc0523d96f072b3fe
PD <phuc.duongdinh@gmail.com>
2017-07-02 18:44:30 -0400

    Updated app.js, user model, users route and added register page

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use('/', index);
-
+app.user('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
