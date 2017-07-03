4a658847428b9b086d8fe329547a39c874704fd3
PD <phuc.duongdinh@gmail.com>
2017-07-03 12:08:14 -0400

    Email implemented

--- services/mail.js
+++ services/mail.js
@@ -1,10 +1,10 @@
 var api_key = process.env.MAILGUN_KEY;
-var domain = 'ott311.esdev.xyz';
+var domain = 'mg.esdev.xyz';
 var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 
 exports.sendEmail = function(data) {
-    //    mailgun.messages().send(data, function(error, body) {
-    //        console.log(body);
-    //    });
+    mailgun.messages().send(data, function(error, body) {
+        console.log(body);
+    });
     console.log(data);
 }
