ebd6615259d86a4f5eac057cd65111ebe92eccfe
Denis <carriere.denis@gmail.com>
2017-07-12 23:18:07 -0400

    Improve docker-compose config

--- models/db.js
+++ models/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
-let {DB_HOST, DB_USER, DB_PASS, NODE_ENV} = process.env;
+let {MONGODB_URI, DB_HOST, DB_USER, DB_PASS, NODE_ENV} = process.env;
 
 // Default Environment Variables
 DB_HOST = DB_HOST || 'localhost';
 
 // Create MongoDB connection
-if (NODE_ENV === 'development' || DB_USER === undefined) {
+if (MONGODB_URI) {
+    const options = { useMongoClient: true };
+    mongoose.connect(MONGODB_URI, options);
+} else if (NODE_ENV === 'development' || DB_USER === undefined) {
     const uri = `mongodb://${DB_HOST}/snaphole`
     const options = { useMongoClient: true };
     mongoose.connect(uri, options);
