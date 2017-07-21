0788ad9d50649b63fe2053ec997a059a5301c071
devisscher <devisscher.thomas@gmail.com>
2017-07-21 12:43:48 -0400

    remove authenticated conn string

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let {MONGODB_URI, DB_HOST, DB_USER, DB_PASS, NODE_ENV} = process.env;
+let { MONGODB_URI, DB_HOST, DB_USER, DB_PASS, NODE_ENV } = process.env;
 
 // Default Environment Variables
 DB_HOST = DB_HOST || 'localhost';
@@ -13,7 +13,11 @@ if (MONGODB_URI) {
     const options = { useMongoClient: true };
     mongoose.connect(uri, options);
 } else {
-    const uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/ott311`
-    const options = { auth: { authdb: "admin" }, useMongoClient: true };
+    const uri = `mongodb://${DB_HOST}/snaphole`
+    const options = { useMongoClient: true };
     mongoose.connect(uri, options);
+    // ignore auth for now.
+    // const uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/ott311`
+    // const options = { auth: { authdb: "admin" }, useMongoClient: true };
+    // mongoose.connect(uri, options);
 }
