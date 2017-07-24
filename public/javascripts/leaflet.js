b20443b43a856c7e8115df585d72c87cf2e1c2aa
devisscher <devisscher.thomas@gmail.com>
2017-07-23 20:43:16 -0400

    map icon

--- public/javascripts/leaflet.js
+++ public/javascripts/leaflet.js
@@ -9,14 +9,14 @@ $.get("/map/json", function(data) {
             layer.bindPopup(feature.properties.size);
         }
     }
-    var greenIcon = L.icon({
-        iconUrl: '/images/pin.png',
-        shadowUrl: '/images/pin.png',
-        iconSize: [38, 95], // size of the icon
-        shadowSize: [50, 64], // size of the shadow
-        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62], // the same for the shadow
-        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    var icon = L.icon({
+        iconUrl: '/images/dot.png',
+        iconRetinaUrl: '/images/dot.png',
+        iconSize: [20, 20],
+        iconAnchor: [9, 10],
+        popupAnchor: [1, -4],
+        shadowUrl: '/images/dot.png',
+        shadowSize: [0, 0]
     });
     var mymap = L.map('mapid').setView([45.2502975, -76.0804332], 9);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -26,12 +26,12 @@ $.get("/map/json", function(data) {
         accessToken: 'pk.eyJ1IjoidG9tbXlkZXYiLCJhIjoiY2oxcDl6Z3R6MDFmbDJ3cGlzMG1rNTltOSJ9.znZbcJMg8q_ujLa4nv-t9w'
     }).addTo(mymap);
     L.geoJSON(holes, {
-        filter: function(feature, layer) {
-            return feature.properties.show_on_map;
-        }
-    }, ).addTo(mymap);
-
-    L.geoJSON(holes, {
+        pointToLayer: function(feature, layer) {
+            console.log(layer, feature);
+            return L.marker(layer, {
+                icon: icon
+            });
+        },
         onEachFeature: onEachFeature
     }).addTo(mymap);
 });
