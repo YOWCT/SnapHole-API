613e3289d4a6125525d45192ea77461e849523ea
devisscher <devisscher.thomas@gmail.com>
2018-01-21 16:27:44 -0500

    Fix passport auth

--- app.js
+++ app.js
@@ -12,41 +12,30 @@ const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 
+// Models
+const User = require('./models/user')
 // Routes
 const index = require('./routes/index')
 const users = require('./routes/users')
-const cookieSession = require('cookie-session')
+//const cookieSession = require('cookie-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
 var app = express()
-app.use(cookieParser())
-app.use(bodyParser())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
 app.use(
-  cookieSession({
-    name: 'session',
-    keys: ['thisismyfirstsecret', 'thisismysecondsecret'],
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  require('express-session')({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
   })
 )
-// app.use(
-//   bodyParser.urlencoded({
-//     limit: '5mb',
-//     extended: true,
-//     keepExtensions: true,
-//     uploadDir: path.join(__dirname, 'uploads')
-//   })
-// )
-
-// app.use(bodyParser.json())
-
-app.use(require('flash')())
-// Passport configuration
-const User = require('./models/user')
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.use(require('flash')())
+// Passport configuration
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
@@ -62,41 +51,27 @@ app.use(function (req, res, next) {
   }
   next()
 })
-// app.use(function (req, res, next) {
-//   if (req.user) {
-//     console.log(`################# user found #################`)
-//     app.locals.user = req.user
-//   } else {
-//     console.log(`################# no user #################`)
-//     app.locals.user = undefined
-//   }
-//   next()
-// })
-app.use(logger('dev'))
 
+app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'uploads')))
 app.use('/', index)
 app.use('/users', users)
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found')
   err.status = 404
   next(err)
 })
-
 //  Including all model files.
 fs.readdirSync(__dirname + '/models').forEach(function (filename) {
   if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
 })
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
   // render the error page
   res.status(err.status || 500)
   res.render('error')
@@ -105,5 +80,4 @@ app.use(function (err, req, res, next) {
 fs.readdirSync(__dirname + '/models').forEach(function (filename) {
   if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
 })
-
 module.exports = app
