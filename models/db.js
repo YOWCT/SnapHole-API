040876f6f735e5a7f169d92dedbb4611be18883f
devisscher <devisscher.thomas@gmail.com>
2018-03-21 20:55:15 -0400

    Use right db

--- models/db.js
+++ models/db.js
@@ -6,6 +6,6 @@ let { DB_PASSWORD } = process.env
 
 // Create MongoDB connection
 // const uri = `mongodb://${DB_HOST}/snaphole`
-const uriAtlas = `mongodb://snaphole:${DB_PASSWORD}@cluster0-shard-00-00-1mxli.mongodb.net:27017,cluster0-shard-00-01-1mxli.mongodb.net:27017,cluster0-shard-00-02-1mxli.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
+const uriAtlas = `mongodb://snaphole:${DB_PASSWORD}@cluster0-shard-00-00-1mxli.mongodb.net:27017,cluster0-shard-00-01-1mxli.mongodb.net:27017,cluster0-shard-00-02-1mxli.mongodb.net:27017/snaphole?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
 const options = { useMongoClient: true }
 mongoose.connect(uriAtlas, options)
