be541417be6abf45e3093f1d90fb0c439a9a3a7e
jsivakumaran <jsivakumaran@outlook.com>
2018-03-25 11:34:57 -0400

    add port to the DB running locally

--- models/db.js
+++ models/db.js
@@ -5,7 +5,7 @@ let { DB_PASSWORD, NODE_ENV } = process.env
 // DB_HOST = DB_HOST || 'localhost'
 
 // Create MongoDB connection
-const uriAtlas = NODE_ENV === 'development' ? `mongodb://localhost/snaphole` : `mongodb://snaphole:${DB_PASSWORD}@cluster0-shard-00-00-1mxli.mongodb.net:27017,cluster0-shard-00-01-1mxli.mongodb.net:27017,cluster0-shard-00-02-1mxli.mongodb.net:27017/snaphole?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
+const uriAtlas = NODE_ENV === 'development' ? `mongodb://localhost:27017/snaphole` : `mongodb://snaphole:${DB_PASSWORD}@cluster0-shard-00-00-1mxli.mongodb.net:27017,cluster0-shard-00-01-1mxli.mongodb.net:27017,cluster0-shard-00-02-1mxli.mongodb.net:27017/snaphole?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
 
 const options = { useMongoClient: true }
 mongoose.connect(uriAtlas, options)
