66da3a2aa1f4fd8c7575a4ff23e48b7b52a8155e
devisscher <devisscher.thomas@gmail.com>
2018-03-25 11:19:01 -0400

    Fix db connection

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-let { DB_PASSWORD } = process.env
+let { DB_PASSWORD, NODE_ENV } = process.env
 
 // Default Environment Variables
 // DB_HOST = DB_HOST || 'localhost'
 
 // Create MongoDB connection
-// const uri = `mongodb://${DB_HOST}/snaphole`
-const uriAtlas = `mongodb://snaphole:${DB_PASSWORD}@cluster0-shard-00-00-1mxli.mongodb.net:27017,cluster0-shard-00-01-1mxli.mongodb.net:27017,cluster0-shard-00-02-1mxli.mongodb.net:27017/snaphole?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
+const uriAtlas = NODE_ENV === 'development' ? `mongodb://localhost/snaphole` : `mongodb://snaphole:${DB_PASSWORD}@cluster0-shard-00-00-1mxli.mongodb.net:27017,cluster0-shard-00-01-1mxli.mongodb.net:27017,cluster0-shard-00-02-1mxli.mongodb.net:27017/snaphole?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
+
 const options = { useMongoClient: true }
 mongoose.connect(uriAtlas, options)
